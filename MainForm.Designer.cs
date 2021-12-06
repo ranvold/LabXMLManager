96db2055743c13cacb45e51f314d5c258fb76f04
Volodymyr Shafran <volodymyr.shafran98@gmail.com>
2021-12-06 20:52:25 +0200

    mod form

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,20 +29,256 @@ namespace LabXMLManager
         /// </summary>
         private void InitializeComponent()
         {
+            this.CheckBoxGroup = new System.Windows.Forms.CheckBox();
+            this.CheckBoxAreasOfThesis = new System.Windows.Forms.CheckBox();
+            this.CheckBoxProfessor = new System.Windows.Forms.CheckBox();
+            this.CheckBoxSchedule = new System.Windows.Forms.CheckBox();
+            this.CheckBoxStage = new System.Windows.Forms.CheckBox();
+            this.ComboBoxGroup = new System.Windows.Forms.ComboBox();
+            this.ComboBoxAreasOfThesis = new System.Windows.Forms.ComboBox();
+            this.ComboBoxProfesor = new System.Windows.Forms.ComboBox();
+            this.ComboBoxSchedule = new System.Windows.Forms.ComboBox();
+            this.ComboBoxStage = new System.Windows.Forms.ComboBox();
+            this.RadioButtonSax = new System.Windows.Forms.RadioButton();
+            this.RadioButtonDom = new System.Windows.Forms.RadioButton();
+            this.RadioButtonLinq = new System.Windows.Forms.RadioButton();
+            this.RichTextBoxDisplay = new System.Windows.Forms.RichTextBox();
+            this.ButtonSearch = new System.Windows.Forms.Button();
+            this.ButtonTransformToHTML = new System.Windows.Forms.Button();
+            this.ButtonClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // CheckBoxGroup
+            // 
+            this.CheckBoxGroup.AutoSize = true;
+            this.CheckBoxGroup.Location = new System.Drawing.Point(46, 88);
+            this.CheckBoxGroup.Margin = new System.Windows.Forms.Padding(4);
+            this.CheckBoxGroup.Name = "CheckBoxGroup";
+            this.CheckBoxGroup.Size = new System.Drawing.Size(67, 23);
+            this.CheckBoxGroup.TabIndex = 0;
+            this.CheckBoxGroup.Text = "Group";
+            this.CheckBoxGroup.UseVisualStyleBackColor = true;
+            // 
+            // CheckBoxAreasOfThesis
+            // 
+            this.CheckBoxAreasOfThesis.AutoSize = true;
+            this.CheckBoxAreasOfThesis.Location = new System.Drawing.Point(46, 156);
+            this.CheckBoxAreasOfThesis.Margin = new System.Windows.Forms.Padding(4);
+            this.CheckBoxAreasOfThesis.Name = "CheckBoxAreasOfThesis";
+            this.CheckBoxAreasOfThesis.Size = new System.Drawing.Size(117, 23);
+            this.CheckBoxAreasOfThesis.TabIndex = 1;
+            this.CheckBoxAreasOfThesis.Text = "Areas of thesis";
+            this.CheckBoxAreasOfThesis.UseVisualStyleBackColor = true;
+            // 
+            // CheckBoxProfessor
+            // 
+            this.CheckBoxProfessor.AutoSize = true;
+            this.CheckBoxProfessor.Location = new System.Drawing.Point(46, 225);
+            this.CheckBoxProfessor.Margin = new System.Windows.Forms.Padding(4);
+            this.CheckBoxProfessor.Name = "CheckBoxProfessor";
+            this.CheckBoxProfessor.Size = new System.Drawing.Size(86, 23);
+            this.CheckBoxProfessor.TabIndex = 2;
+            this.CheckBoxProfessor.Text = "Professor";
+            this.CheckBoxProfessor.UseVisualStyleBackColor = true;
+            // 
+            // CheckBoxSchedule
+            // 
+            this.CheckBoxSchedule.AutoSize = true;
+            this.CheckBoxSchedule.Location = new System.Drawing.Point(46, 291);
+            this.CheckBoxSchedule.Margin = new System.Windows.Forms.Padding(4);
+            this.CheckBoxSchedule.Name = "CheckBoxSchedule";
+            this.CheckBoxSchedule.Size = new System.Drawing.Size(83, 23);
+            this.CheckBoxSchedule.TabIndex = 3;
+            this.CheckBoxSchedule.Text = "Schedule";
+            this.CheckBoxSchedule.UseVisualStyleBackColor = true;
+            // 
+            // CheckBoxStage
+            // 
+            this.CheckBoxStage.AutoSize = true;
+            this.CheckBoxStage.Location = new System.Drawing.Point(46, 358);
+            this.CheckBoxStage.Margin = new System.Windows.Forms.Padding(4);
+            this.CheckBoxStage.Name = "CheckBoxStage";
+            this.CheckBoxStage.Size = new System.Drawing.Size(62, 23);
+            this.CheckBoxStage.TabIndex = 4;
+            this.CheckBoxStage.Text = "Stage";
+            this.CheckBoxStage.UseVisualStyleBackColor = true;
+            // 
+            // ComboBoxGroup
+            // 
+            this.ComboBoxGroup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBoxGroup.FormattingEnabled = true;
+            this.ComboBoxGroup.Location = new System.Drawing.Point(180, 86);
+            this.ComboBoxGroup.Margin = new System.Windows.Forms.Padding(4);
+            this.ComboBoxGroup.Name = "ComboBoxGroup";
+            this.ComboBoxGroup.Size = new System.Drawing.Size(265, 27);
+            this.ComboBoxGroup.TabIndex = 5;
+            // 
+            // ComboBoxAreasOfThesis
+            // 
+            this.ComboBoxAreasOfThesis.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBoxAreasOfThesis.FormattingEnabled = true;
+            this.ComboBoxAreasOfThesis.Location = new System.Drawing.Point(180, 154);
+            this.ComboBoxAreasOfThesis.Margin = new System.Windows.Forms.Padding(4);
+            this.ComboBoxAreasOfThesis.Name = "ComboBoxAreasOfThesis";
+            this.ComboBoxAreasOfThesis.Size = new System.Drawing.Size(265, 27);
+            this.ComboBoxAreasOfThesis.TabIndex = 6;
+            // 
+            // ComboBoxProfesor
+            // 
+            this.ComboBoxProfesor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBoxProfesor.FormattingEnabled = true;
+            this.ComboBoxProfesor.Location = new System.Drawing.Point(180, 223);
+            this.ComboBoxProfesor.Margin = new System.Windows.Forms.Padding(4);
+            this.ComboBoxProfesor.Name = "ComboBoxProfesor";
+            this.ComboBoxProfesor.Size = new System.Drawing.Size(265, 27);
+            this.ComboBoxProfesor.TabIndex = 7;
+            // 
+            // ComboBoxSchedule
+            // 
+            this.ComboBoxSchedule.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBoxSchedule.FormattingEnabled = true;
+            this.ComboBoxSchedule.Location = new System.Drawing.Point(180, 289);
+            this.ComboBoxSchedule.Margin = new System.Windows.Forms.Padding(4);
+            this.ComboBoxSchedule.Name = "ComboBoxSchedule";
+            this.ComboBoxSchedule.Size = new System.Drawing.Size(265, 27);
+            this.ComboBoxSchedule.TabIndex = 8;
+            // 
+            // ComboBoxStage
+            // 
+            this.ComboBoxStage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBoxStage.FormattingEnabled = true;
+            this.ComboBoxStage.Location = new System.Drawing.Point(180, 356);
+            this.ComboBoxStage.Margin = new System.Windows.Forms.Padding(4);
+            this.ComboBoxStage.Name = "ComboBoxStage";
+            this.ComboBoxStage.Size = new System.Drawing.Size(265, 27);
+            this.ComboBoxStage.TabIndex = 9;
+            // 
+            // RadioButtonSax
+            // 
+            this.RadioButtonSax.AutoSize = true;
+            this.RadioButtonSax.Location = new System.Drawing.Point(46, 501);
+            this.RadioButtonSax.Margin = new System.Windows.Forms.Padding(4);
+            this.RadioButtonSax.Name = "RadioButtonSax";
+            this.RadioButtonSax.Size = new System.Drawing.Size(58, 23);
+            this.RadioButtonSax.TabIndex = 10;
+            this.RadioButtonSax.TabStop = true;
+            this.RadioButtonSax.Text = "SAX";
+            this.RadioButtonSax.UseVisualStyleBackColor = true;
+            // 
+            // RadioButtonDom
+            // 
+            this.RadioButtonDom.AutoSize = true;
+            this.RadioButtonDom.Location = new System.Drawing.Point(208, 501);
+            this.RadioButtonDom.Margin = new System.Windows.Forms.Padding(4);
+            this.RadioButtonDom.Name = "RadioButtonDom";
+            this.RadioButtonDom.Size = new System.Drawing.Size(64, 23);
+            this.RadioButtonDom.TabIndex = 11;
+            this.RadioButtonDom.TabStop = true;
+            this.RadioButtonDom.Text = "DOM";
+            this.RadioButtonDom.UseVisualStyleBackColor = true;
+            // 
+            // RadioButtonLinq
+            // 
+            this.RadioButtonLinq.AutoSize = true;
+            this.RadioButtonLinq.Location = new System.Drawing.Point(380, 501);
+            this.RadioButtonLinq.Margin = new System.Windows.Forms.Padding(4);
+            this.RadioButtonLinq.Name = "RadioButtonLinq";
+            this.RadioButtonLinq.Size = new System.Drawing.Size(65, 23);
+            this.RadioButtonLinq.TabIndex = 12;
+            this.RadioButtonLinq.TabStop = true;
+            this.RadioButtonLinq.Text = "LINQ";
+            this.RadioButtonLinq.UseVisualStyleBackColor = true;
+            // 
+            // RichTextBoxDisplay
+            // 
+            this.RichTextBoxDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RichTextBoxDisplay.Location = new System.Drawing.Point(493, 86);
+            this.RichTextBoxDisplay.Margin = new System.Windows.Forms.Padding(4);
+            this.RichTextBoxDisplay.Name = "RichTextBoxDisplay";
+            this.RichTextBoxDisplay.Size = new System.Drawing.Size(729, 548);
+            this.RichTextBoxDisplay.TabIndex = 13;
+            this.RichTextBoxDisplay.Text = "";
+            // 
+            // ButtonSearch
+            // 
+            this.ButtonSearch.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonSearch.Location = new System.Drawing.Point(46, 542);
+            this.ButtonSearch.Name = "ButtonSearch";
+            this.ButtonSearch.Size = new System.Drawing.Size(399, 42);
+            this.ButtonSearch.TabIndex = 14;
+            this.ButtonSearch.Text = "Search";
+            this.ButtonSearch.UseVisualStyleBackColor = true;
+            // 
+            // ButtonTransformToHTML
+            // 
+            this.ButtonTransformToHTML.Location = new System.Drawing.Point(46, 590);
+            this.ButtonTransformToHTML.Name = "ButtonTransformToHTML";
+            this.ButtonTransformToHTML.Size = new System.Drawing.Size(195, 44);
+            this.ButtonTransformToHTML.TabIndex = 15;
+            this.ButtonTransformToHTML.Text = "Transform to HTML";
+            this.ButtonTransformToHTML.UseVisualStyleBackColor = true;
+            // 
+            // ButtonClear
+            // 
+            this.ButtonClear.Location = new System.Drawing.Point(247, 590);
+            this.ButtonClear.Name = "ButtonClear";
+            this.ButtonClear.Size = new System.Drawing.Size(198, 44);
+            this.ButtonClear.TabIndex = 16;
+            this.ButtonClear.Text = "Clear";
+            this.ButtonClear.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.ButtonClear);
+            this.Controls.Add(this.ButtonTransformToHTML);
+            this.Controls.Add(this.ButtonSearch);
+            this.Controls.Add(this.RichTextBoxDisplay);
+            this.Controls.Add(this.RadioButtonLinq);
+            this.Controls.Add(this.RadioButtonDom);
+            this.Controls.Add(this.RadioButtonSax);
+            this.Controls.Add(this.ComboBoxStage);
+            this.Controls.Add(this.ComboBoxSchedule);
+            this.Controls.Add(this.ComboBoxProfesor);
+            this.Controls.Add(this.ComboBoxAreasOfThesis);
+            this.Controls.Add(this.ComboBoxGroup);
+            this.Controls.Add(this.CheckBoxStage);
+            this.Controls.Add(this.CheckBoxSchedule);
+            this.Controls.Add(this.CheckBoxProfessor);
+            this.Controls.Add(this.CheckBoxAreasOfThesis);
+            this.Controls.Add(this.CheckBoxGroup);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.Text = "JournalOfThesis";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox CheckBoxGroup;
+        private System.Windows.Forms.CheckBox CheckBoxAreasOfThesis;
+        private System.Windows.Forms.CheckBox CheckBoxProfessor;
+        private System.Windows.Forms.CheckBox CheckBoxSchedule;
+        private System.Windows.Forms.CheckBox CheckBoxStage;
+        private System.Windows.Forms.ComboBox ComboBoxGroup;
+        private System.Windows.Forms.ComboBox ComboBoxAreasOfThesis;
+        private System.Windows.Forms.ComboBox ComboBoxProfesor;
+        private System.Windows.Forms.ComboBox ComboBoxSchedule;
+        private System.Windows.Forms.ComboBox ComboBoxStage;
+        private System.Windows.Forms.RadioButton RadioButtonSax;
+        private System.Windows.Forms.RadioButton RadioButtonDom;
+        private System.Windows.Forms.RadioButton RadioButtonLinq;
+        private System.Windows.Forms.RichTextBox RichTextBoxDisplay;
+        private System.Windows.Forms.Button ButtonSearch;
+        private System.Windows.Forms.Button ButtonTransformToHTML;
+        private System.Windows.Forms.Button ButtonClear;
     }
 }
 
