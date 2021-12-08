83d524ba0bf5f134ae53a1c7f49804444e24017f
Volodymyr Shafran <volodymyr.shafran98@gmail.com>
2021-12-09 00:20:21 +0200

    simplified SAX

--- Sax.cs
+++ Sax.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace LabXMLManager
@@ -29,54 +26,48 @@ namespace LabXMLManager
                         string thesisTopic = "";
                         string schedule = "";
                         string auxiliaryMaterials = "";
-                        string stage = "";                            
+                        string stage = "";
 
                         if (xmlReader.Name.Equals("FULLNAME"))
                         {
                             fullName = xmlReader.Value;
                             xmlReader.MoveToNextAttribute();
-
-                            if (xmlReader.Name.Equals("GROUP") && (xmlReader.Value.Equals(std.Group) || (String.IsNullOrEmpty(std.Group))))
-                            {
-                                group = xmlReader.Value;
-                                xmlReader.MoveToNextAttribute();
-
-                                if (xmlReader.Name.Equals("AREA_OF_THESIS") && (xmlReader.Value.Equals(std.AreaOfThesis) || (String.IsNullOrEmpty(std.AreaOfThesis))))
-                                {
-                                    areaOfThesis = xmlReader.Value;
-                                    xmlReader.MoveToNextAttribute();
-
-                                    if (xmlReader.Name.Equals("PROFESSOR") && (xmlReader.Value.Equals(std.Professor) || (String.IsNullOrEmpty(std.Professor))))
-                                    {
-                                        professor = xmlReader.Value;
-                                        xmlReader.MoveToNextAttribute();
-
-                                        if (xmlReader.Name.Equals("THESIS_TOPIC"))
-                                        {
-                                            thesisTopic = xmlReader.Value;
-                                            xmlReader.MoveToNextAttribute();
-
-                                            if (xmlReader.Name.Equals("SCHEDULE") && (xmlReader.Value.Equals(std.Schedule) || (String.IsNullOrEmpty(std.Schedule))))
-                                            {
-                                                schedule = xmlReader.Value;
-                                                xmlReader.MoveToNextAttribute();
-
-                                                if (xmlReader.Name.Equals("AUXILIARY_MATERIALS"))
-                                                {
-                                                    auxiliaryMaterials = xmlReader.Value;
-                                                    xmlReader.MoveToNextAttribute();
-
-                                                    if (xmlReader.Name.Equals("STAGE") && (xmlReader.Value.Equals(std.Stage) || (String.IsNullOrEmpty(std.Stage))))
-                                                    {
-                                                        stage = xmlReader.Value;
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
                         }
+                        if (xmlReader.Name.Equals("GROUP") && (xmlReader.Value.Equals(std.Group) || (String.IsNullOrEmpty(std.Group))))
+                        {
+                            group = xmlReader.Value;
+                            xmlReader.MoveToNextAttribute();
+                        }
+                        if (xmlReader.Name.Equals("AREA_OF_THESIS") && (xmlReader.Value.Equals(std.AreaOfThesis) || (String.IsNullOrEmpty(std.AreaOfThesis))))
+                        {
+                            areaOfThesis = xmlReader.Value;
+                            xmlReader.MoveToNextAttribute();
+                        }
+                        if (xmlReader.Name.Equals("PROFESSOR") && (xmlReader.Value.Equals(std.Professor) || (String.IsNullOrEmpty(std.Professor))))
+                        {
+                            professor = xmlReader.Value;
+                            xmlReader.MoveToNextAttribute();
+                        }
+                        if (xmlReader.Name.Equals("THESIS_TOPIC"))
+                        {
+                            thesisTopic = xmlReader.Value;
+                            xmlReader.MoveToNextAttribute();
+                        }
+                        if (xmlReader.Name.Equals("SCHEDULE") && (xmlReader.Value.Equals(std.Schedule) || (String.IsNullOrEmpty(std.Schedule))))
+                        {
+                            schedule = xmlReader.Value;
+                            xmlReader.MoveToNextAttribute();
+                        }
+                        if (xmlReader.Name.Equals("AUXILIARY_MATERIALS"))
+                        {
+                            auxiliaryMaterials = xmlReader.Value;
+                            xmlReader.MoveToNextAttribute();
+                        }
+                        if (xmlReader.Name.Equals("STAGE") && (xmlReader.Value.Equals(std.Stage) || (String.IsNullOrEmpty(std.Stage))))
+                        {
+                            stage = xmlReader.Value;
+                        }
+
                         if (group != "" && areaOfThesis != "" && professor != "" && schedule != "" && stage != "")
                         {
                             var filteredStudent = new Student()
